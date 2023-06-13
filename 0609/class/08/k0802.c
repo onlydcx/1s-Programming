047f4c34a59f4a04d306964e375f5dd55dd9e2f9
onlydcx <rin.hanaoka@icloud.com>
2023-06-13 17:33:42 +0900

    フィボナッチ数列

--- 0609/class/08/k0802.c
+++ 0609/class/08/k0802.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
 int fibo(int n) {
-    
+    if(n < 2) return n;
+    else return fibo(n - 1) + fibo(n - 2);
 }
 
 int main(void) {
+    for(int i = 0; i < 10; i++) {
+        printf("%d ",fibo(i));
+    }
+    printf("\n");
     return 0;
 }
